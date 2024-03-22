72358eb5f6b1bb3da2dc535d5a03bc072ae60397
Snekye <dimitrigranit22@gmail.com>
2024-03-22 09:37:22 +0100

    Base rudimentaire du component Table

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -3,14 +3,21 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Attribute\Route;
+
+use App\Entity\Classroom;
+use Doctrine\ORM\EntityManagerInterface;
+use App\Controller\BaseController;
 
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'home')]
-    public function home()
+    public function home(EntityManagerInterface $m)
     {
-        return $this->render("home.html.twig");
+        $data = $m->getRepository(Classroom::class)->findAll();
+        $data = BaseController::ObjsToArray($data);
+        return $this->render("public/home.html.twig", [
+            "data" => $data,
+        ]);
     }
 }
