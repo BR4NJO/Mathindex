9222dbe1fa2bcc261343ab70e0ce55609fec159d
Alexandre Brunet <aquasword60@gmail.com>
2024-03-22 11:09:45 +0100

    MAJ branche alex

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -11,6 +11,6 @@ class HomeController extends AbstractController
     #[Route('/', name: 'home')]
     public function home()
     {
-        return $this->render("home.html.twig");
+        return $this->render("public/home.html.twig");
     }
 }
