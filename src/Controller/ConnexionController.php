a5712f145e89cb395bada174213092699a488388
Alexandre Brunet <aquasword60@gmail.com>
2024-03-25 10:28:12 +0100

    Commit Page connexion

--- src/Controller/ConnexionController.php
+++ src/Controller/ConnexionController.php
@@ -6,13 +6,35 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
+use App\Form\ConnexionType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
+
+use App\Repository\UserRepository;
+use App\Entity\User;
+
 class ConnexionController extends AbstractController
 {
-    #[Route('/connexion', name: 'app_connexion')]
-    public function index(): Response
+    
+    /*======================AJOUT ADMIN======================*/
+
+    #[Route('/connexion', name: 'connexion')]
+    public function connexion(Request $request, EntityManagerInterface $entityManager): Response
     {
-        return $this->render('connexion/index.html.twig', [
-            'controller_name' => 'ConnexionController',
+        $user = new User();
+        $form = $this->createForm(ConnexionType::class, $user);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+ 
+
+            // Redirection après ajout réussi
+            return $this->redirectToRoute('/admin');
+        }
+
+        return $this->render('public/connexion.html.twig', [
+            'form' => $form->createView(),
         ]);
     }
 }
