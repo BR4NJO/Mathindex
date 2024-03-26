ceb09fa65882d9227b3d57130ae9d864619af02b
marcusWeb04 <marcusfavernay@gmail.com>
2024-03-26 15:58:17 +0100

    création de la page d'accueil avec les liens vers la page mathématique

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -5,20 +5,14 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Attribute\Route;
 
-use App\Entity\Exercise;
 use Doctrine\ORM\EntityManagerInterface;
-use App\Trait\ObjsToArrayTrait;
 
 class HomeController extends AbstractController
 {
-    use ObjsToArrayTrait;
     #[Route('/', name: 'home')]
-    public function home(EntityManagerInterface $m)
+    public function home(EntityManagerInterface $entity)
     {
-        $data = $m->getRepository(Exercise::class)->findAll();
-        $data = $this->ObjsToArray($data);
         return $this->render("public/home.html.twig", [
-            "data" => $data,
         ]);
     }
 }
--- src/Controller/MathController.php
+++ src/Controller/MathController.php
@@ -6,17 +6,33 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Attribute\Route;
 
 use App\Entity\Exercise;
-use Doctrine\ORM\EntityManagerInterface;
+use App\Trait\PaginationTrait;
 use App\Trait\ObjsToArrayTrait;
+use App\Repository\ExerciceRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 
 class MathController extends AbstractController
 {
+    use PaginationTrait;
     use ObjsToArrayTrait;
+
     #[Route('/math', name: 'math')]
-    public function math(EntityManagerInterface $m)
+    public function math(EntityManagerInterface $entity): Response
     {
-        $data = $m->getRepository(Exercise::class)->findAll();
+        $data = $entity->getRepository(Exercise::class)->findAll();
         $data = $this->ObjsToArray($data);
+        return $this->render("public/math.html.twig", [
+            "data" => $data,
+        ]);
+    }
+
+    #[Route('/math/{id}', name: 'mathShow', methods:'GET')]
+    public function mathShow(EntityManagerInterface $entity, string $id): Response
+    {
+        $data = $entity->getRepository(Exercise::class)->findAll();
         return $this->render("public/math.html.twig", [
             "data" => $data,
         ]);
