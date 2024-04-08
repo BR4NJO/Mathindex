60af2a41b3abe638058cb3699a21e994b00b7feb
marcusWeb04 <marcusfavernay@gmail.com>
2024-04-08 16:37:21 +0200

    ajout dans la page mathématique pour faire en sorte que lorsque le tableau des exexercice n'affiche seleument les 3 exercices avec les id les plusgrand

--- src/Controller/ExerciceController.php
+++ src/Controller/ExerciceController.php
@@ -2,17 +2,61 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Exercise;
+use App\Entity\Thematic;
+use App\Entity\Classroom;
+use App\Form\ExerciseSearchFormType;
+// use App\src\Repository\ExerciseRepository;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 use Doctrine\ORM\EntityManagerInterface;
 
 class ExerciceController extends AbstractController
 {
     #[Route('/exercice', name: 'exercice')]
-    public function home(EntityManagerInterface $entity)
+    // ExerciseRepository $exerciceRepository
+    public function home(Request $request, EntityManagerInterface $entity)
     {
-        return $this->render("public/exerice.html.twig", [
+
+        $form = $this->createForm(ExerciseSearchFormType::class,[
+            'method' => 'POST',
+        ]);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted()){
+                if($form->isValid()){
+                    return $this->redirectToRoute('exerciceFound',[
+                        'id' => $id,
+                ]);
+            }
+        }
+
+        $nbExerciceTrouver = 0;
+
+        return $this->render('public/exercice.html.twig', [
+            'form' => $form,
+            'nbExerciceTrouver' => $nbExerciceTrouver,
+        ]);
+    }
+
+    #[Route('/exercice/{id}', name:'exerciceFound')]
+    public function exerciceFound(Request $request, EntityManagerInterface $entity, string $id)
+    {
+        $exercice = new Exercise();
+
+        $form = $this->createForm(ExerciseSearchFormType::class,[
+            'method' => 'POST',
+        ]);
+
+        $nbExerciceTrouver = 0;
+
+        return $this->render('public/exercice.html.twig', [
+            'form' => $form,
+            'nbExerciceTrouver' => $nbExerciceTrouver,
         ]);
     }
 }
--- src/Form/ExerciseSearchFormType.php
+++ src/Form/ExerciseSearchFormType.php
@@ -2,27 +2,34 @@
 
 namespace App\Form;
 
-use App\Entity\Classroom;
 use App\Entity\Exercise;
 use App\Entity\Thematic;
-use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use App\Entity\Classroom;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
 class ExerciseSearchFormType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('difficulty')
             ->add('classroom', EntityType::class, [
                 'class' => Classroom::class,
-                'choice_label' => 'id',
+                'choice_label' => 'name',
             ])
+
             ->add('thematic', EntityType::class, [
                 'class' => Thematic::class,
-                'choice_label' => 'id',
+                'choice_label' => 'name',
+            ])
+
+            ->add('keywords')
+
+            ->add('Rechercher', SubmitType::class,[
+
             ])
         ;
     }
