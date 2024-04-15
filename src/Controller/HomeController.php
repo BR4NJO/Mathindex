4780197460b3d806ae340f29355d917f54ef0270
Alexandre Brunet <aquasword60@gmail.com>
2024-04-15 12:22:31 +0200

    Commit Session + deconnexion

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -4,15 +4,20 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Attribute\Route;
-
 use Doctrine\ORM\EntityManagerInterface;
 
 class HomeController extends AbstractController
 {
+    #[Route('/logout', name: 'app_logout', methods: ['GET'])]
+    public function logout(): never
+    {
+        // controller can be blank: it will never be called!
+        throw new \Exception('Don\'t forget to activate logout in security.yaml');
+    }
+
     #[Route('/', name: 'home')]
     public function home(EntityManagerInterface $entity)
     {
-        return $this->render("public/home.html.twig", [
-        ]);
+        return $this->render("public/home.html.twig", []);
     }
 }
