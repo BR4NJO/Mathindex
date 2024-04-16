4038a046f66f5164c2ad2383666dad7e0b964b5a
marcusWeb04 <marcusfavernay@gmail.com>
2024-04-16 15:21:56 +0200

    ajout de la page recherche et création de la page mes exercices

--- src/Controller/ExerciceController.php
+++ src/Controller/ExerciceController.php
@@ -32,14 +32,41 @@ class ExerciceController extends AbstractController
 
             // Call the search method in Exercise repository
             $exercises = $entityManager->getRepository(Exercise::class)->search($data);
-
             $nbExerciceTrouver = count($exercises);
         }
-    
-        return $this->render('public/exercice.html.twig', [
-            'form' => $form,
-            'nbExerciceTrouver' => $nbExerciceTrouver ?? 0,
-            'exercises' => $exercises,
-        ]);
+
+        if($nbExerciceTrouver > 7){
+            return $this->render('public/exercice.html.twig', [
+                'form' => $form,
+                'nbExerciceTrouver' => $nbExerciceTrouver ?? 0,
+                'exercises' => $exercises,
+                'paginate' => true,
+            ]);
+        }else{
+
+            // pagination
+            $count = $entity->getRepository(Exercise::class)->count([]);
+            $countPerPage = 6;
+            
+            $currentPage = $request->query->getInt('page',1);
+            $countPages = ceil($count/$countPerPage);
+            
+            // On vérifie que la page renseignée dans l'url est valide, si ce n'est pas le cas on génère une 404.
+            if ($currentPage > $countPages || $currentPage <= 0) {
+                throw $this->createNotFoundException();
+            }
+        
+            $exercices = $entity->getRepository(Exercise::class)->paginate($currentPage, $countPerPage);
+
+            return $this->render('public/exercice.html.twig', [
+                'form' => $form,
+                'paginate' => false,
+                'exercises' => $exercises,
+                'countPages' => $countPages,
+                'currentPage' => $currentPage,
+                "exercicespaginate" => $exercicespaginate,
+                'nbExerciceTrouver' => $nbExerciceTrouver ?? 0,
+            ]);
+        }
     }
 }
