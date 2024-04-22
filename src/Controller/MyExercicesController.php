c3e9df1d51607d4db34eaa9ab2108c58c4a215fd
marcusWeb04 <marcusfavernay@gmail.com>
2024-04-22 16:58:25 +0200

    correction mineur

--- src/Controller/MyExercicesController.php
+++ src/Controller/MyExercicesController.php
@@ -26,44 +26,19 @@ class MyExercicesController extends AbstractController
         $user = $this->getUser();
 
         if (empty($user)){
-            return $this->render("404.html.twig", []);
-        }
-
-        // Récupérer les exercices liés à l'utilisateur connecté
-        $userExercices = $entity->getRepository(Exercise::class)->findBy(['user' => $user]);
-
-        // Récupérer tous les exercices
-        $allExercices = $entity->getRepository(Exercise::class)->findAll();
-
-        // Fusionner les deux tableaux d'exercices
-        $exercices = array_merge($userExercices, $allExercices);
-
-        if (empty($user)){
             throw $this->createNotFoundException();
         }
      
         // On récupére le nombre d'exercice étant lié au user
-            $exerciseUser = $entity->getRepository(Exercise::class)
+            $exercices = $entity->getRepository(Exercise::class)
             ->findBy(['user' => $user]);
             // dd($exerciseUser);
 
-        $exercices = $entity->getRepository(Exercise::class)->paginate($currentPage, $countPerPage);
-
-        // tableau: nouveau exerice
-        $data = $entity->getRepository(Exercise::class)
-            ->findBy([], ['id' => 'DESC'], 3);
-        $data = $this->ObjsToArray($data);
-
         // tableau avec pagination
         $exercicespaginate = $this->ObjsToArray($exercices);
-        dd($exercicespaginate);
 
         return $this->render("public/myexercices.html.twig", [
-            "data" => $data,
-            "exercicespaginate" => $exercicespaginate,
             'exercices' => $exercices,
-            'countPages' => $countPages,
-            'currentPage' => $currentPage,
         ]);
     }
 }
