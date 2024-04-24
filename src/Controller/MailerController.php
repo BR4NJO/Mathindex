0dc856806421a171390b3f3776d5a1ee8bbdf908
Alexandre Brunet <aquasword60@gmail.com>
2024-04-24 14:19:33 +0200

    Comit mailer

--- src/Controller/MailerController.php
+++ src/Controller/MailerController.php
@@ -4,28 +4,43 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Mailer\MailerInterface;
-use Symfony\Component\Mime\Email;
 use Symfony\Component\Routing\Attribute\Route;
+
+use App\Form\MailerType;
+use Symfony\Component\Mime\Email;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Mailer\MailerInterface;
 
 class MailerController extends AbstractController
 {
-    #[Route('/email')]
-    public function sendEmail(MailerInterface $mailer)
+    #[Route('/mail', name: 'mail')]
+    public function sendEmail(Request $request, MailerInterface $mailer): Response
     {
-        $email = (new Email())
-            ->from('hello@example.com')
-            ->to('you@example.com')
-            //->cc('cc@example.com')
-            //->bcc('bcc@example.com')
-            //->replyTo('fabien@example.com')
-            //->priority(Email::PRIORITY_HIGH)
-            ->subject('Time for Symfony Mailer!')
-            ->text('Sending emails is fun again!')
-            ->html('<p>See Twig integration for better HTML integration!</p>');
+        $form = $this->createForm(MailerType::class);
+    
+        $form->handleRequest($request);
 
-        $mailer->send($email);
+        if($form->isSubmitted() && $form->isValid()){
+            $data = $form->getData();
 
-        // ...
+            $address = $data['email'];
+            $content = $data['content'];
+
+            $email = (new Email())
+                ->from($address)
+                ->to('aquasword60@gmail.com')
+                ->subject('Demande Mot de Passe oublié')
+                ->text($content);
+
+            $mailer->send($email);
+
+            return $this->redirectToRoute('home');
+        }
+
+        return $this->render("public/mail.html.twig", [
+            'controller_name' => 'MailerController',
+            'formulaire' => $form
+        ]
+        );
     }
 }
